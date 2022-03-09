941f2520d81e0322b8e7f9cd051e3e51c97ff6c3
lluaca <77507435+galvestonco@users.noreply.github.com>
2022-03-09 11:08:10 -0800

    Update STDBASE_LICENSE_RENEWAL_ISSUANCE.js

--- Scripts/STDBASE_LICENSE_RENEWAL_ISSUANCE.js
+++ Scripts/STDBASE_LICENSE_RENEWAL_ISSUANCE.js
@@ -365,3 +365,36 @@ function activeLicense(capid)
 	}
 	return false;
 } 
+
+function logDebugObject(myObject) {
+/*
+usage - logDebugObject(object)
+
+author - Michael Zachry
+created - 10/10/2018
+
+updates
+10/11/2018 - initial version
+
+*/
+  //list the methods
+  try {
+    logDebug("object is is a " + myObject.getClass());
+    logDebug("object has the following methods:");
+    for (x in myObject) {
+      if (typeof(myObject[x]) == "function" ) {
+        logDebug("  " + x);
+      }
+    }
+
+    //list the properties and values    
+    logDebug("object has the following properties and values:");
+    for (x in myObject) {
+      if (typeof(myObject[x]) != "function" ) {
+        logDebug("  " + x + " = " + myObject[x]);
+      }
+    }
+  } catch (err) {
+    logDebug("A JavaScript Error occured: " + err.message);
+  }
+}
