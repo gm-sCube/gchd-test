f6c923a8149b256a3090a26cc7a5bc502a165669
lluaca <77507435+galvestonco@users.noreply.github.com>
2022-03-09 16:58:20 -0800

    Update clearNotificationInfo.js

--- Scripts/clearNotificationInfo.js
+++ Scripts/clearNotificationInfo.js
@@ -1,7 +1,7 @@
 /*
 script to clear Next Notification and Next Notification Date
 */
-var parentCapId = getRenewalParentCapID(itemCapId); // This is renewal cap
+var parentCapId = getRenewalParentCapID(capId); // This is renewal cap
 var parentCapIdStr = parentCapId.getCustomID();
 logDebug("Parent cap: " + parentCapIdStr);
 editAppSpecific("Next Notification","",parentCapId);
