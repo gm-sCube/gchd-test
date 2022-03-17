e652a77c32677d8a70614ac66478d88c112ecc25
lluaca <77507435+galvestonco@users.noreply.github.com>
2022-03-17 11:06:13 -0700

    Update STDBASE_ASSET_AUTOMATION.js
    To force the use of the sequence mask when creating the asset

--- Scripts/STDBASE_ASSET_AUTOMATION.js
+++ Scripts/STDBASE_ASSET_AUTOMATION.js
@@ -182,7 +182,7 @@ function assetAutomation(rules) {
 		newAssetDataModel = newAssetDataModel.getOutput().getAssetDataModel();
 	}
 	if (!refAssetExist) {
-		masterAsset = fillMasterModelFromASI(rules, true);
+		masterAsset = fillMasterModelFromASI(rules, false);
 
 		if (rules.action.copyAppNameToAssetName) {
 			var capBasicInfo = aa.cap.getCapBasicInfo(capId).getOutput();
