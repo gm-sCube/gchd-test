c8a7a19d3d17c48f7ff52c75f5e0879713459c51
lluaca <77507435+galvestonco@users.noreply.github.com>
2022-03-18 12:25:30 -0700

    Update STDBASE_ASSET_AUTOMATION.js

--- Scripts/STDBASE_ASSET_AUTOMATION.js
+++ Scripts/STDBASE_ASSET_AUTOMATION.js
@@ -195,6 +195,12 @@ function assetAutomation(rules) {
 		}
 
 		newAssetDataModel.setAssetMaster(masterAsset);
+
+		logDebug("logDebugObject(masterAsset)"
+		logDebugObject(masterAsset);
+		logDebug("logDebugObject(newAssetDataModel)");
+		logDebugObject(newAssetDataModel);
+
 		try {
 			assetSeqNum = assetDataService.createAssetDataWithoutEvent(newAssetDataModel);
 		} catch (ex) {
@@ -494,4 +500,37 @@ function findAssetByMasterAssetModel(assetMasterModel) {
 		return asts.get(0);
 	}
 	return null;
+}
+
+function logDebugObject(myObject) {
+/*
+usage - logDebugObject(object)
+
+author - Michael Zachry
+created - 10/10/2018
+
+updates
+10/11/2018 - initial version
+
+*/
+  //list the methods
+  try {
+    logDebug("object is is a " + myObject.getClass());
+    logDebug("object has the following methods:");
+    for (x in myObject) {
+      if (typeof(myObject[x]) == "function" ) {
+        logDebug("  " + x);
+      }
+    }
+
+    //list the properties and values    
+    logDebug("object has the following properties and values:");
+    for (x in myObject) {
+      if (typeof(myObject[x]) != "function" ) {
+        logDebug("  " + x + " = " + myObject[x]);
+      }
+    }
+  } catch (err) {
+    logDebug("A JavaScript Error occured: " + err.message);
+  }
 }
