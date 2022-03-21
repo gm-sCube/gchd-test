561c447ea1b7a80dee09f88bf31cff3e3a411261
lluaca <77507435+galvestonco@users.noreply.github.com>
2022-03-21 15:10:51 -0700

    Update STDBASE_ASSET_AUTOMATION.js

--- Scripts/STDBASE_ASSET_AUTOMATION.js
+++ Scripts/STDBASE_ASSET_AUTOMATION.js
@@ -464,7 +464,8 @@ function fillMasterModelFromASI(rules, fillAssetId) {
 	if (fillAssetId) {
 		if(rules.action.linkParent){
 			vParentCapId = getParentByCapId(capId);
-			logDebug("rules.action.maskName: ", rules.action.maskName);
+			
+			logDebug("typeof(rules.action.maskName): ", typeof(rules.action.maskName));
 			// if (rules.action.maskName) {
 				// assetMasterModel.setG1AssetID(myGetNextSequence(rules.action.maskName));
 			// }else{
